acfebbac8cfa0f56135f0c1eed62006e134e752e
Jorge González <yo@jorgeglz.io>
2020-06-25 17:46:27 -0500

    :shrug:

--- pages/about.vue
+++ pages/about.vue
@@ -1,37 +1,3 @@
 <template>
-  <div class="container">
-    <div>
-      <h1>Hello from <span class="name">{{ name }}</span>.</h1>
-      <p><NLink to="/" class="button--grey">Back home</NLink></p>
-    </div>
-  </div>
+  <h1 class="text-lg font-bold">About View</h1>
 </template>
-
-<script>
-export default {
-  asyncData() {
-    return {
-      name: (process.server ? 'server' : 'client')
-    }
-  }
-}
-</script>
-
-<style scoped>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-h1 {
-  font-weight: 400;
-}
-.name {
-  color: #00C48D;
-}
-p {
-  margin-top: 20px;
-}
-</style>
